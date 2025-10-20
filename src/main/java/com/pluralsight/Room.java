5c544ddbaec64ff8ac77fc6a6c77627ba0c72130
montasia harris <mharris@my.yearupunited.com>
2025-10-20 14:28:37 -0400

    wrkbkwk4 hotel operations oop exercise

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,7 +1,59 @@
 package com.pluralsight;
 
 public class Room {
-    public static void main(String[] args) {
+            private int NumberOfBeds;
+            private double Price;
+            private boolean Occupied;
+            private boolean Dirty;
 
-    }
-}
+
+
+    public Room( int NumberOfBeds, double Price, boolean Occupied, boolean Dirty){
+                this.NumberOfBeds = NumberOfBeds;
+                this.Price = Price;
+                this.Occupied = Occupied;
+                this.Dirty = Dirty;
+
+            }
+
+            public int getNumberOfBeds () {
+                return NumberOfBeds;
+            }
+
+            public double getPrice () {
+                return Price;
+            }
+
+            public boolean isOccupied () {
+                return Occupied;
+            }
+
+            public boolean isDirty () {
+                return Dirty;
+            }
+
+            public boolean isAvailable () {
+                return !Occupied && !Dirty;
+            }
+
+            // Example main method to test
+            public static void main (String[]args){
+                Room room1 = new Room(2, 150.00, false, false);
+
+                System.out.println("Beds: " + room1.getNumberOfBeds());
+                System.out.println("Price: $" + room1.getPrice());
+                System.out.println("Occupied: " + room1.isOccupied());
+                System.out.println("Dirty: " + room1.isDirty());
+                System.out.println("Available: " + room1.isAvailable());
+            }
+        }
+
+
+
+
+
+
+
+
+
+
